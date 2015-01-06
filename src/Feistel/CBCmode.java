15b177f8c7ae5a962fcd15a957ff242042ca623d
asafsh <asias22@gmail.com>
2015-01-06 10:46:20 +0200

    V.5
    Added shit and stuff

---
+++ src/Feistel/CBCmode.java
@@ -0,0 +1,148 @@
+package Feistel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.commons.codec.binary.Base64;
+
+
+public class CBCmode {
+	
+	private byte[] iv = new byte[FeistelFunction.BLOCK_SIZE];
+    private byte[] plaintext;
+    private byte[] ciphertext;
+
+    public void CBC() {
+    	readFiles(); // Read the plaintext and ciphertext files.
+    	encrypt();
+    	
+    }
+    
+    // Read the plaintext file.
+    private void readFiles() {
+		File plaintext_file = new File("p.txt");
+		File ciphertext_file = new File("c.txt");
+		FileInputStream stream;
+		try {
+			stream = new FileInputStream(plaintext_file);
+			stream.read(plaintext);
+			stream.close();
+			stream = new FileInputStream(ciphertext_file);
+			stream.read(ciphertext);
+			stream.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("No file name \"p.txt\" or \"c.txt\" was found.");
+			System.exit(1);
+		} catch (IOException e) {
+			System.out.println("Problome reading the \"p.txt\" or \"c.txt\" file.");
+			System.exit(1);
+		}
+}
+    /// Set the Initialization Vector.
+    public void setIv() { 
+    	// Set the VI to be 0101.... at the size of the block. 
+    	for (int i = 0; i < (FeistelFunction.BLOCK_SIZE); i++) { 
+    		iv[i] = (byte) (i % 2); 
+    	} 
+    } 
+    
+    // Encrypt the given text.
+    public void encrypt() {
+    	try {
+    		// Create the file to print the result in.
+			FileOutputStream out = new FileOutputStream("ciphertext-result.txt");
+			// Create the first ciphertext with the IV.
+			
+			byte[] nextPart = FeistelFunction.getHalf(plaintext,0, 7);
+			byte[] res = FeistelFunction.xor(iv, nextPart);
+			res = FeistelFunction.encryptBlock(res);
+			// Decode in Base 64.
+			byte[] encodedBytes = Base64.encodeBase64(res);
+			System.out.println("First Part:  " + new String(encodedBytes));
+			out.write(encodedBytes);
+			
+			// Create the rest of the ciphertext.
+			int index = 8;
+			while (index < (plaintext.length - 8)) {
+				nextPart = FeistelFunction.getHalf(plaintext, index, index + 8);
+				res = FeistelFunction.xor(res, nextPart);
+				res = FeistelFunction.encryptBlock(res);
+				encodedBytes = Base64.encodeBase64(res);
+				System.out.println("part " + (index/8) + ": " + new String(encodedBytes));
+				out.write(encodedBytes);
+				index = index + 8;
+			}
+			// Check if there is more bytes to encrypt.
+			if (index < plaintext.length) {
+				nextPart = FeistelFunction.getHalf(plaintext, index, plaintext.length - 1);
+				int neededAdd = 8 - nextPart.length;
+				for (int i=0; i < neededAdd; i++) {
+					nextPart[nextPart.length + i] = 0;
+				}
+				res = FeistelFunction.xor(res, nextPart);
+				res = FeistelFunction.encryptBlock(res);
+				encodedBytes = Base64.encodeBase64(res);
+				System.out.println("Last part: " + new String(encodedBytes));
+				out.write(res);
+			}
+			out.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not create the file: \"ciphertext-result.txt\".");
+			System.exit(1);
+		} catch (IOException e) {
+			System.out.println("Could not write to the file: \"ciphertext-result.txt\".");
+			System.exit(1);
+		}
+    	
+    }
+    
+
+    public void decrypt() {
+       	try {
+    		// Create the file to print the result in.
+			FileOutputStream out = new FileOutputStream("plaintext-result.txt");
+			// Create the first plaintext with the IV.
+			byte[] nextPart = FeistelFunction.getHalf(ciphertext,0, 7);
+			nextPart = FeistelFunction.decryptBlock(nextPart);
+			byte[] res = FeistelFunction.xor(iv, nextPart);
+			// Decode in Base 64.
+			byte[] decodedBytes = Base64.decodeBase64(res);
+			System.out.println("First Part: " + new String(decodedBytes));
+			out.write(res);
+			// Create the rest of the ciphertext.
+			int index = 8;
+			while (index < (ciphertext.length - 8)) {
+				nextPart = FeistelFunction.getHalf(ciphertext, index, index + 8);
+				nextPart = FeistelFunction.decryptBlock(nextPart);
+				res = FeistelFunction.xor(res, nextPart);
+				decodedBytes = Base64.decodeBase64(res);
+				System.out.println("part " + (index/8) + ": " + new String(decodedBytes));
+				out.write(res);
+				index = index + 8;
+			}
+			// Check if there is more bytes to encrypt.
+			if (index < ciphertext.length) {
+				nextPart = FeistelFunction.getHalf(ciphertext, index, ciphertext.length - 1);
+				int neededAdd = 8 - nextPart.length;
+				for (int i=0; i < neededAdd; i++) {
+					nextPart[nextPart.length + i] = 0;
+				}
+				nextPart = FeistelFunction.decryptBlock(nextPart);
+				res = FeistelFunction.xor(res, nextPart);
+				decodedBytes = Base64.decodeBase64(res);
+				System.out.println("Last part: " + new String(decodedBytes));
+				out.write(res);
+			}
+			out.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not create the file: \"plaintext-result.txt\".");
+			System.exit(1);
+		} catch (IOException e) {
+			System.out.println("Could not write to the file: \"plaintext-result.txt\".");
+			System.exit(1);
+		}
+    }	
+}
+
--- src/Feistel/FeistelFunction.java
+++ src/Feistel/FeistelFunction.java
@@ -1,16 +1,18 @@
 package Feistel;
+
 import java.util.ArrayList;
+
+import Feistel.SubkeyGeneration;
 
 public class FeistelFunction {
 
 	final static int BLOCK_SIZE = 64;
 	final static int KEY_SIZE = 56;
-	final int PERMUTATED_BLOCK = 48;
-	private byte[] plaintext;
-	private int currentRound = 0;
+	final static int PERMUTATED_BLOCK = 48;
+	private static int currentRound = 0;
 
 	//change numbers
-	private static final byte[] Etable = {
+	private static final byte[] table = {
 		32, 1,  2,  3,  4,  5,   
 		12, 13, 14, 15, 16, 17,
 		16, 17, 18, 19, 20, 21,
@@ -21,157 +23,160 @@ public class FeistelFunction {
 		28, 29, 30, 31, 32, 1
 	};
 
-	//gets Ri and a sub-key and 
+	private static final byte[] s_box1 = {
+		10, 9, 0, 14, 6, 15, 3, 5, 1, 13, 7, 12, 4, 11, 8, 2,
+		7, 13, 9, 0, 3, 6, 4, 10, 2, 8, 5, 12, 14, 11, 1, 15,
+		6, 13, 4, 8, 9, 15, 0, 3, 1, 11, 12, 2, 5, 14, 10, 7,
+		10, 1, 13, 8, 9, 7, 4, 7, 15, 3, 14, 11, 5, 11, 12, 2
+	};
+
+	private static final byte[] s_box2 = {
+		14, 4, 1, 13, 15, 2, 8, 11, 10, 8, 3, 12, 6, 5, 9, 7,
+		15, 4, 7, 14, 13, 2, 13, 1, 6, 10, 12, 11, 3, 9, 5, 8,
+		4, 1, 14, 13, 8, 6, 2, 11, 15, 9, 12, 7, 3, 10, 0, 5,
+		15, 12, 2, 8, 9, 4, 1, 7, 11, 5, 14, 3, 10, 0, 13, 6
+	};
+
+	private static final byte[] s_box3 = {
+		15, 8, 1, 14, 6, 3, 9, 11, 4, 7, 3, 2, 12, 0, 13, 5,
+		13, 3, 7, 4, 15, 8, 2, 12, 14, 10, 1, 10, 9, 6, 11,
+		14, 0, 7, 10, 11, 4, 1, 13, 5, 12, 8, 6, 3, 15, 2, 9,
+		8, 13, 1, 10, 15, 2, 11, 6, 3, 4, 6, 12, 7, 0, 14, 5
+	};
+
+	private static final byte[] s_box4 = {
+		13, 8, 2, 4, 6, 11, 15, 11, 10, 3, 9, 0, 14, 12, 7, 5,
+		15, 1, 10, 8, 7, 3, 13, 4, 12, 6, 5, 11, 14, 0, 9, 2,
+		11, 4, 7, 1, 12, 9, 14, 0, 2, 6, 0, 13, 10, 5, 3, 15,
+		1, 14, 2, 7, 4, 10, 13, 10, 15, 9, 3, 12, 6, 0, 11, 5
+	};
+
+	private static final byte[] s_box5 = {
+		13, 7, 3, 14, 6, 0, 10, 9, 2, 1, 5, 8, 11, 12, 15, 4,
+		8, 13, 5, 11, 6, 15, 3, 0, 4, 2, 7, 12, 10, 1, 9, 14,
+		10, 9, 6, 12, 0, 11, 13, 7, 15, 3, 1, 14, 2, 5, 4, 8,
+		15, 3, 6, 0, 1, 10, 13, 9, 4, 8, 5, 11, 2, 7, 14, 2
+	};
+
+	private static final byte[] s_box6 = {
+		11, 2, 4, 14, 0, 8, 15, 3, 13, 7, 9, 10, 5, 1, 6, 12,
+		13, 0, 11, 4, 7, 1, 9, 10, 3, 14, 5, 2, 12, 6, 8, 15,
+		11, 13, 1, 4, 12, 2, 7, 3, 10, 14, 15, 8, 6, 0, 9, 5,
+		11, 13, 1, 6, 10, 4, 7, 9, 5, 15, 0, 14, 3, 2, 8, 12
+	};
+
+	private static final byte[] s_box7 = {
+		12, 4, 2, 7, 1, 10, 11, 8, 6, 5, 15, 3, 14, 0, 9, 13,
+		11, 14, 12, 2, 7, 4, 13, 5, 1, 15, 0, 10, 9, 3, 6, 8,
+		2, 4, 11, 1, 10, 7, 13, 15, 8, 6, 5, 0, 14, 9, 3, 12,
+		12, 11, 8, 7, 1, 2, 14, 13, 15, 6, 10, 9, 5, 4, 3, 0
+	};
+
+	private static final byte[] s_box8 = {
+		12, 10, 15, 1, 2, 9, 8, 6, 0, 3, 13, 14, 4, 11, 7, 5,
+		15, 10, 4, 7, 2, 9, 12, 5, 8, 6, 13, 1, 14, 0, 11, 3,
+		14, 15, 9, 2, 5, 8, 3, 12, 0, 7, 4, 13, 10, 11, 6, 1,
+		3, 4, 12, 2, 5, 9, 10, 15, 14, 1, 11, 6, 7, 0, 13, 8
+	};
+
+	private static byte[] sboxes(byte[] Ri) {
+		for (int i = 0; i < Ri.length; i++) {
+			byte rByte = Ri[i];
+			// take the LSB
+			int j = ((rByte >> 7) & 1) | (rByte >> 3);
+			int k = (rByte >> 2);
+
+		}
+		return null;
+	}
+
+	//gets Ri and a sub-key and  
 	public byte[] fFunction(byte[] Ri, byte[] key) {
-		byte newR; // = permutation function
+		// Permuted to 48 bits
+		byte[] newR = new byte[table.length];
+		for (int i = 0; i < table.length; i++) {
+			newR[i] = Ri[table[i] - 1];
+		}
 		newR = xor(newR, key);
-		//newR = 
-		
+
+		//s box is a must, we need to go back to 32!
+		return null;
 	}
 
 	public static byte[] perm32To48(byte[] in, byte[] table) {
-
+		return null;
 	}
 
-	public byte[] encryptBlock(byte[] block, byte[] key) {
+	public static byte[] encryptBlock(byte[] block) {
 		byte[] permutatedBlock = new byte[BLOCK_SIZE];
 		byte[] Ri = new byte[BLOCK_SIZE / 2];
 		byte[] Li = new byte[BLOCK_SIZE / 2];
 
-		permutatedBlock = perm32To48(block, Etable);
-		//Li = getHalf(block, 0, PERMUTATED_BLOCK / 2 - 1);
-		//Ri = getHalf(block, PERMUTATED_BLOCK / 2, PERMUTATED_BLOCK - 1);
+		currentRound++;
 
+		permutatedBlock = perm32To48(block, table);
+		Li = getHalf(block, 0, PERMUTATED_BLOCK / 2 - 1);
+		Ri = getHalf(block, PERMUTATED_BLOCK / 2, PERMUTATED_BLOCK - 1);
+
+		// do 16 rounds
 		for (int i = 0; i < 16; i++) {
 			byte[] oldR = Ri;
-			//Ri = fFunction(Ri, makeKey(currentRound));
+			Ri = fFunction(Ri, makeKey(currentRound));
 			Ri = xor(Ri, Li);
 			Li = oldR;
 		}
+		
+		return unite(Li, Ri);
+
 	}
-	
-	public static byte[] getHalf(byte[] block, int start, int finish) {
+
+	public static byte[] decryptBlock(byte[] block) {
+		byte[] permutatedBlock = new byte[BLOCK_SIZE];
+		byte[] Ri = new byte[BLOCK_SIZE / 2];
+		byte[] Li = new byte[BLOCK_SIZE / 2];
+
+		currentRound++;
+
+		permutatedBlock = perm32To48(block, table);
+		Li = getHalf(block, 0, PERMUTATED_BLOCK / 2 - 1);
+		Ri = getHalf(block, PERMUTATED_BLOCK / 2, PERMUTATED_BLOCK - 1);
+		
+		// do 16 rounds
+		for (int i = 16; i > 0; i--) {
+			byte[] oldR = Ri;
+			Ri = fFunction(Ri, makeKey(i));
+			Ri = xor(Ri, Li);
+			Li = oldR;
+		}
+		
+		return unite(Li, Ri);
+	}
+
+	static byte[] xor(byte[] R, byte[] L) {
+		byte[] res = new byte[R.length];
+		for (int i = 0; i < res.length; i++) {
+			res[i] = (byte)(R[i] ^ L[i]);
+		}
+		return null;
+	}
+
+	static byte[] getHalf(byte[] block, int start, int finish) {
 		byte[] res = new byte[finish-start];
 		for (int i=0; i < (finish-start); i++) {
 			res[i] = block[start + i];
 		}
 		return res;
 	}
-	
-	private static byte[] xor(byte[] R, byte[] L) {
-		byte[] res = new byte[R.length];
-		for (int i = 0; i < res.length; i++) {
-			res[i] = (byte)(R[i] ^ L[i]);
-		}
-	}
 
-	//public ArrayList <byte[]> encodeFeistel(String plaintext);
-	//public String decodeFeistel(ArrayList <byte[]>);      
-	String encodeString ="";         
-	public byte[] functionF(){ 
-		byte[] returnvalue = new byte[2]; 
-		returnvalue [0] = 1;
-		returnvalue [1] = 0;
-		return returnvalue;                
-	} 
-
-	public String encode(String plaintext, int length) {
-		byte[] ri,li,temp;
-		ArrayList<String> hilfsText = block(plaintext,length);
-		//System.out.print(hilfsText);
-		ArrayList<String> encoded = new ArrayList<String>();
-		for(String block:hilfsText) {
-			String L0 = block.substring(0, 2);
-			String R0 = block.substring(2, 4);
-			System.out.println(L0 +""+ R0);
-			ri = R0.getBytes();
-			li = L0.getBytes();
-			temp = functionF();
-			//mit Key XOR
-			for(int i = 0;i < ri.length;i++) {
-				temp[i] = (byte) (ri[i]^temp[i]);
-			}
-			//mit LI XOR
-			for(int i = 0;i < ri.length;i++) {
-				temp[i] = (byte) (temp[i]^li[i]);                       
-			}
-			li = ri;
-			System.out.println(temp+""+ri);
-			encoded.add(li.toString()+temp.toString());
+	private static byte[] unite(byte[] l, byte[] r) {
+		byte[] union = new byte[BLOCK_SIZE];
+		for (int i = 0; i < l.length; i++) {
+			union[i] = l[i];
 		}
-		return Block2String(encoded);
-	}
-	/*
-	 * Decoden
-	 */
-	public String decode(String codedText, int length)
-	{
-		byte[] ri,li,temp;
-		ArrayList<String> hilfsText = block(codedText,length);
-		System.out.print(hilfsText);
-		ArrayList<String> encoded = new ArrayList<String>();
-		for(String block:hilfsText) {
-			String L0 = block.substring(2, 4);
-			String R0 = block.substring(0, 2);
-			ri = R0.getBytes();
-			li = L0.getBytes();
-			temp = functionF();
-			//mit Key XOR
-			for(int i = 0;i < ri.length;i++) {
-				temp[i] = (byte) (temp[i]^ri[i]);
-			}
-			//mit LI XOR
-			for(int i = 0;i < ri.length;i++)
-			{
-				temp[i] = (byte) (li[i]^temp[i]);
-			}
-			li = ri;
-			encoded.add(ri.toString()+temp.toString());
+		for (int j = 0; j < r.length; j++) {
+			union[j + (BLOCK_SIZE / 2)] = r[j]; 
 		}
-		return Block2String(encoded);
-	}
-	/*
-	 * Blockifizieren
-	 */
-	//Blockifizieren
-	static ArrayList<String> block(String plaintext, int key)
-	{
-		ArrayList<String> blocks= new ArrayList<String>();
-		while(plaintext.length()>=key)
-		{
-			blocks.add(plaintext.subSequence(0, key).toString());
-			plaintext = plaintext.subSequence(key,plaintext.length()).toString();
-		}
-		if(plaintext.length() == 0)
-		{                         
-		} 
-		else 
-		{ 
-			while(plaintext.length() < key) 
-			{
-				plaintext += (char)randomGenerator();
-			}
-			blocks.add(plaintext.subSequence(0, plaintext.length()).toString());
-		}
-		return blocks;
-	}      
-
-	private static int randomGenerator(){                  
-		Random generator = new Random();
-		int pad = generator.nextInt(255);        
-		return pad;        
-	}
-	//Aus Arraylist wieder String machen
-	private static String Block2String(ArrayList<String> cryptText) {
-		String text="";
-		for(String block:cryptText) {
-			text += block;
-		}
-		return text;
-	}
-
-	// A sub-key generator
-	private static int[] subKeyGenerator(String key) {
+		return union;
 
 	}
 }
